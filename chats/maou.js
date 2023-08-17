ba312171aa8bddf81941aa4e67fe02efc3ad97f7
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2023-08-18 01:50:55 +0900

    逆だったかもしれねえ

--- chats/maou.js
+++ chats/maou.js
@@ -1,29 +1,11 @@
 //@ts-check
-import { PREFIX } from "../constants.js";
+import { MAO_REGEX } from "../constants.js";
+import { shuffleWord, useMessage } from "../utils.js";
 /** @typedef {import("../type.d.ts").ChatFunction} ChatFunction */
 
 /** @type {ChatFunction} */
 export default async ({ message }) => {
-  if (message.content.startsWith(PREFIX)) {
-    const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
-    const command = args.shift()?.toLowerCase();
-    switch (command) {
-      case "help":
-        await message.channel.send({
-          embeds: [
-            {
-              title: "ヘルプ",
-              description: "なにもないよ",
-              fields: [
-                {
-                  name: "ヘルプ",
-                  value: "`help`",
-                },
-              ],
-            },
-          ],
-        });
-        break;
-    }
-  }
+  const { reply } = useMessage(message);
+
+  if (MAO_REGEX.test(message.content)) await reply(shuffleWord(["把握"]));
 };
--- chats/prefix.js
+++ chats/prefix.js
@@ -1,11 +1,29 @@
 //@ts-check
-import { MAO_REGEX } from "../constants.js";
-import { shuffleWord, useMessage } from "../utils.js";
+import { PREFIX } from "../constants.js";
 /** @typedef {import("../type.d.ts").ChatFunction} ChatFunction */
 
 /** @type {ChatFunction} */
 export default async ({ message }) => {
-  const { reply } = useMessage(message);
-
-  if (MAO_REGEX.test(message.content)) await reply(shuffleWord(["把握"]));
+  if (message.content.startsWith(PREFIX)) {
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+    const command = args.shift()?.toLowerCase();
+    switch (command) {
+      case "help":
+        await message.channel.send({
+          embeds: [
+            {
+              title: "ヘルプ",
+              description: "なにもないよ",
+              fields: [
+                {
+                  name: "ヘルプ",
+                  value: "`help`",
+                },
+              ],
+            },
+          ],
+        });
+        break;
+    }
+  }
 };
