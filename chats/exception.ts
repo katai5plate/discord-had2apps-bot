c6dd9f4feae865cc0b3149820f56ca0075bce8a4
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2026-04-11 01:40:45 +0900

    エラーハンドリングを修正

--- chats/exception.ts
+++ chats/exception.ts
@@ -2,9 +2,20 @@ import { ChatFunction } from "../types";
 import { useMessage } from "../utils";
 
 const chat: ChatFunction = async ({ message, error }) => {
-  const { reply } = useMessage(message);
+  console.log("EXCEPTION AT", new Date().toLocaleString());
+  if (message.system) {
+    console.log("システムメッセージエラー揉み消し:", message.content);
+    return;
+  }
+  const { reply, post } = useMessage(message);
 
-  await reply(`問題が発生したよ。${error?.name}: ${error?.message}`);
+  try {
+    const content = `問題が発生したよ。${error?.name}: ${error?.message}`;
+    await reply(content);
+  } catch {
+    const content = `問題が発生したよ。${error?.name}: ${error?.message}\nあとリプライにも失敗したよ。`;
+    await post(content);
+  }
   console.log(error?.stack);
 };
 
